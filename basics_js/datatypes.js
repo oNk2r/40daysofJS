56edb9c0e2402785f901b583f5cb44f4c5a23aaa
Onkar <193019074+oNk2r@users.noreply.github.com>
2025-08-18 19:50:23 +0000

    variables and datatypes

---
+++ basics_js/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+
+//alert() //for browser
+
+console.log(3+3); console.log("hello")
+
+
+let name="om"
+let age=18
+let isLoggedIn =false
+
+// number==> 2 to power 53
+// Bigint
+// string=>""
+// boolean =>t/false
+// null==>standalone value
+//undefined==>
+//symbol==>unique
+
+//object
+console.log(typeof "om");
